d513436751fd9744dfc8a31244acdb1cf045f8a3
Christian Montero Avalos <christian.montero@myper.com.pe>
2022-02-06 02:28:47 -0500

    Agregar archivos de proyecto.

---
+++ TelePSocial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TelePSocial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
